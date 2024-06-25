34d276f7db4e8f1639d9da2037773c6ade80268f
TomCube <121310847+yomcube@users.noreply.github.com>
2024-06-25 17:44:22 -0500

    More actions stuff

--- build_actions.sh
+++ build_actions.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/bash
-CC=wine CW/mwcceppc.exe
-CFLAGS="I- -i KamekInclude -i GameSource -i \"GameSource/MarioKartWii\" -i PulsarEngine -i BKW -opt all -inline auto -enum int -fp hard -sdata 0 -sdata2 0 -maxerrors 1 -func_align 4
+mkdir BKW_
+for f in * **; do
+	mv "$f" "BKW_/$f"
+done
+git clone "https://github.com/MelgMKW/Pulsar.git"
+cd BKW_
+mv BKW ../Pulsar/BKW
+mv -f PulsarEngine/200cc.cpp ../PulsarEngine/Race/200cc.cpp
+cd ../Pulsar
+
+CC="wine CW/mwcceppc.exe"
+CFLAGS="-I- -i KamekInclude -i GameSource -i \"GameSource/MarioKartWii\" \
+-i PulsarEngine -i BKW -opt all -inline auto -enum int -fp hard -sdata 0 \
+-sdata2 0 -maxerrors 1 -func_align 4"
 CPPFILES=""
 
 mkdir build
-$CC "$CFLAGS" -c -o "build/kamek.o" "$ENGINE/kamek.cpp"
+"$CC" "$CFLAGS" -c -o "build/kamek.o" "$ENGINE/kamek.cpp"
 
 OBJECTS=
 for f in BKW/**.cpp; do
@@ -17,4 +29,4 @@ for f in PulsarEngine/*.cpp PulsarEngine/**/*.cpp; do
 done
 
 echo "Linking..."
-dotnet "KamekLinker/bin/Release/net7.0/Kamek.exe" "build/kamek.o" $OBJECTS -dynamic -externals="GameSource/symbols.txt" -versions="GameSource/versions.txt" -output-combined=build/Code.pul
+dotnet "KamekLinker/bin/Release/net8.0/Kamek.exe" "build/kamek.o" $OBJECTS -dynamic -externals="GameSource/symbols.txt" -versions="GameSource/versions.txt" -output-combined=build/Code.pul
