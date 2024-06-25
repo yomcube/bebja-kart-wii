ffb4563530b8ddf741cf5c2113f614e420ff162c
TomCube <121310847+yomcube@users.noreply.github.com>
2024-06-25 17:04:38 -0500

    Use mono

--- build_actions.sh
+++ build_actions.sh
@@ -3,16 +3,17 @@ CFLAGS="-I- -i KamekInclude -i GameSource -i \"GameSource/MarioKartWii\" -i Puls
 CPPFILES=""
 
 mkdir build
-$CC $CFLAGS -c -o "build/kamek.o" "$ENGINE/kamek.cpp"
+$CC "$CFLAGS" -c -o "build/kamek.o" "$ENGINE/kamek.cpp"
 
 OBJECTS=
 for f in BKW/**.cpp; do
-	$CC $CFLAGS -c -o "build/${f%.cpp}.o" "$f"
+	$CC "$CFLAGS" -c -o "build/${f%.cpp}.o" "$f"
  	OBJECTS="$OBJECTS ${f%.cpp}"
 done
 for f in PulsarEngine/*.cpp PulsarEngine/**/*.cpp; do
- 	$CC $CFLAGS -c -o "build/${f%.cpp}.o" "$f"
+ 	$CC "$CFLAGS" -c -o "build/${f%.cpp}.o" "$f"
 	OBJECTS="$OBJECTS ${f%.cpp}"
 done
+
 echo "Linking..."
-dotnet "KamekLinker/bin/Release/net7.0/Kamek.exe" "build/kamek.o" $OBJECTS -dynamic -externals="GameSource/symbols.txt" -versions="GameSource/versions.txt" -output-combined=build/Code.pul
+mono "KamekLinker/bin/Release/net7.0/Kamek.exe" "build/kamek.o" $OBJECTS -dynamic -externals="GameSource/symbols.txt" -versions="GameSource/versions.txt" -output-combined=build/Code.pul
