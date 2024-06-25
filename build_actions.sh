746e83b4acf52535c3cb16a1196ddc4ebb80d89c
TomCube <121310847+yomcube@users.noreply.github.com>
2024-06-25 16:56:10 -0500

    Use net7.0Release

--- build_actions.sh
+++ build_actions.sh
@@ -1,5 +1,5 @@
 CC=wine CW/mwcceppc.exe
-CFLAGS=-I- -i "KamekInclude" -i "GameSource" -i "GameSource/MarioKartWii" -i PulsarEngine -i BKW -opt all -inline auto -enum int -fp hard -sdata 0 -sdata2 0 -maxerrors 1 -func_align 4
+CFLAGS="-I- -i KamekInclude -i GameSource -i \"GameSource/MarioKartWii\" -i PulsarEngine -i BKW -opt all -inline auto -enum int -fp hard -sdata 0 -sdata2 0 -maxerrors 1 -func_align 4"
 CPPFILES=""
 
 mkdir build
@@ -13,4 +13,4 @@ for f in PulsarEngine/*.cpp PulsarEngine/**/*.cpp; do
  	OBJECTS="$OBJECTS $f"
 done
 echo "Linking..."
-dotnet "KamekLinker/bin/Debug/net7.0/Kamek.exe" "build/kamek.o" $OBJECTS -dynamic -externals="GameSource/symbols.txt" -versions="GameSource/versions.txt" -output-combined=build/Code.pul
+dotnet "KamekLinker/bin/Release/net7.0/Kamek.exe" "build/kamek.o" $OBJECTS -dynamic -externals="GameSource/symbols.txt" -versions="GameSource/versions.txt" -output-combined=build/Code.pul
