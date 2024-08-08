714832f6bbe0d349117e7da1d9e5f1615c14deab
Bebja <148148114+Bebja@users.noreply.github.com>
2024-08-08 13:20:32 +0200

    Update BKWMiscCodes.cpp
    try to fix Mirror Mode TTs ig

--- BKW/BKWMiscCodes.cpp
+++ BKW/BKWMiscCodes.cpp
@@ -81,21 +81,15 @@ static void MirrorModeTTs() {
 u8 dis = Pulsar::Settings::Mgr::GetSettingValue(static_cast<Pulsar::Settings::Type>(SETTINGSTYPE_BKW), SETTINGBKW_RADIO_MIRRORMODETTS);
 	if (dis!= 1) {
 			asm(
-				  "loc_0x0:"
-				  "cmpwi r24, 0x2;"
- 				  "bne- loc_0xC;"
- 				  "li r7, 0x1;"
-				  "loc_0xC:"
-				  "stw r7, 2960(r31);"
+				"cmpwi r24,2;"
+  				 "bne end;"
+  				 "li r7,1;" // this flag should always be 0 in TTs
+   				 end:
+  				  "stw r7,0xb90(r31);"
 				);
 			}
 		}
 kmCall(0x8053056C, MirrorModeTTs);
-
-
-
-
-
 
 //Disable HUD [Bully]
 static void DisableHUD() {
