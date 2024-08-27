9a19ed7c02adbb0dc73f28b41029afaae7d9e5cf
Bebja <148148114+Bebja@users.noreply.github.com>
2024-08-27 16:42:32 +0200

    Update BKWMiscCodes.cpp
    C2 Code (Mega Mushroom Increased FOV [TheLordScruffy])

--- BKW/BKWMiscCodes.cpp
+++ BKW/BKWMiscCodes.cpp
@@ -76,6 +76,23 @@ kmWrite16(0x8083A7B6, 0x00004E20);
 //Allow pausing before the race starts [Sponge]
 kmWrite32(0x80856A28, 0x48000050);
 
+//Mega Mushroom Increased FOV [TheLordScruffy]
+kmCallDefAsm(0x805793) {
+loc_0x0:
+	lwz r4, 0(r28)
+		lwz r29, 36(r4)
+		cmpwi r29, 0x0
+		beq - loc_0x28
+		lwz r3, 4(r4)
+		lwz r3, 12(r3)
+		rlwinm.r3, r3, 0, 16, 16
+		beq - loc_0x28
+		lis r0, 0x41F0
+		stw r0, 288(r29)
+
+		loc_0x28:
+}
+
 //Mirror Mode TTs
 static void MirrorModeTTs() {
 u8 dis = Pulsar::Settings::Mgr::GetSettingValue(static_cast<Pulsar::Settings::Type>(SETTINGSTYPE_BKW), SETTINGBKW_RADIO_MIRRORMODETTS);
